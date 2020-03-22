16626c2522a123756f74c7a039018896fa3c917e
cyshdyx@126.com <cysdyx@126.com>
2020-03-22 15:41:57 +0800

    项目结构

---
+++ src/main/java/com/cysh/common/tools/CommonToolsApplication.java
@@ -0,0 +1,13 @@
+package com.cysh.common.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cysh/common/tools/CommonToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cysh.common.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
